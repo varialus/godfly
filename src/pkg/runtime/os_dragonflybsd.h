2003aeee8341a34908d83bc226db63fa320a663d
Varialus <varialus@gmail.com>
2013-08-07 20:31:08 -0600

    Added os_dragonflybsd.c based on OpenBSD and changed os_dragonflybsd.h to be based on OpenBSD to prevent undefined name error

--- src/pkg/runtime/os_dragonflybsd.h
+++ src/pkg/runtime/os_dragonflybsd.h
@@ -1,26 +1,22 @@
-// Copyright 2011 The Go Authors. All rights reserved.
+// Copyright 2010 The Go Authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 #define SS_DISABLE 4
 
-int32	runtime·thr_new(ThrParam*, int32);
+#define SIG_BLOCK 1
+#define SIG_UNBLOCK 2
+#define SIG_SETMASK 3
+
+struct sigaction;
+
 void	runtime·sigpanic(void);
-void	runtime·sigaltstack(Sigaltstack*, Sigaltstack*);
-struct	sigaction;
-void	runtime·sigaction(int32, struct sigaction*, struct sigaction*);
-void	runtime·sigprocmask(Sigset *, Sigset *);
+
 void	runtime·setitimer(int32, Itimerval*, Itimerval*);
+void	runtime·sigaction(int32, struct sigaction*, struct sigaction*);
+void	runtime·sigaltstack(Sigaltstack*, Sigaltstack*);
+Sigset	runtime·sigprocmask(int32, Sigset);
 int32	runtime·sysctl(uint32*, uint32, byte*, uintptr*, byte*, uintptr);
 
-
 #define	NSIG 33
-#define	SI_USER	0x10001
-
-#define RLIMIT_AS 10
-typedef struct Rlimit Rlimit;
-struct Rlimit {
-	int64	rlim_cur;
-	int64	rlim_max;
-};
-int32	runtime·getrlimit(int32, Rlimit*);
+#define	SI_USER	0
