37b9123f7655d316b9feddb371f5043949e46594
Varialus <varialus@gmail.com>
2013-08-11 16:23:16 -0600

    Revert to the FreeBSD way of locking

--- src/pkg/runtime/lock_sema.c
+++ src/pkg/runtime/lock_sema.c
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin netbsd openbsd plan9 windows dragonflybsd
+// +build darwin netbsd openbsd plan9 windows
 
 #include "runtime.h"
 #include "stack.h"
