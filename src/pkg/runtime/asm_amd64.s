f612661dc46729f5aff3ceb423df3bb6d0e24a85
Varialus <varialus@gmail.com>
2013-08-11 16:31:04 -0600

    Revert the skipping of settls and ldt0setup

--- src/pkg/runtime/asm_amd64.s
+++ src/pkg/runtime/asm_amd64.s
@@ -52,9 +52,6 @@ needtls:
 	// skip TLS setup on Plan 9
 	CMPL	runtime·isplan9(SB), $1
 	JEQ ok
-	// skip TLS setup on DragonFly BSD
-	CMPL	runtime·isdragonflybsd(SB), $1
-	JEQ ok
 
 	LEAQ	runtime·tls0(SB), DI
 	CALL	runtime·settls(SB)
