81c7239db9fe9f28be0184fcb62885bbdab05b47
Varialus <varialus@gmail.com>
2013-08-17 01:39:45 -0600

    Fixed undefined: _Socklen

--- src/pkg/syscall/ztypes_dragonfly_amd64.go
+++ src/pkg/syscall/ztypes_dragonfly_amd64.go
@@ -1,5 +1,5 @@
-// Created by cgo -godefs - DO NOT EDIT
-// cgo -godefs types_freebsd.go
+// Not Created by cgo -godefs - DO EDIT
+// cgo -godefs types_dragonfly.go
 
 package syscall
 
@@ -12,3 +12,6 @@ type Timeval struct {
 	Sec  int64
 	Usec int64
 }
+
+// Copied from ztypes_freebsd_amd64.go
+type _Socklen uint32
