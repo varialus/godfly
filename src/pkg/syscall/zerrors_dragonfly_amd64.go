7154e6560a28582ad2f7d189bd61460712fbfe43
Varialus <varialus@gmail.com>
2013-08-19 15:02:27 -0600

    Fix syntax

--- src/pkg/syscall/zerrors_dragonfly_amd64.go
+++ src/pkg/syscall/zerrors_dragonfly_amd64.go
@@ -1591,15 +1591,15 @@ const (
 // * Types
 // */
 //#define	SOCK_STREAM	1		/* stream socket */
-	SOCK_STREAM	1		/* stream socket */
+	SOCK_STREAM	= 1		/* stream socket */
 //#define	SOCK_DGRAM	2		/* datagram socket */
-	SOCK_DGRAM	2		/* datagram socket */
+	SOCK_DGRAM	= 2		/* datagram socket */
 //#define	SOCK_RAW	3		/* raw-protocol interface */
-	SOCK_RAW	3		/* raw-protocol interface */
+	SOCK_RAW	= 3		/* raw-protocol interface */
 //#define	SOCK_RDM	4		/* reliably-delivered message */
-	SOCK_RDM	4		/* reliably-delivered message */
+	SOCK_RDM	= 4		/* reliably-delivered message */
 //#define	SOCK_SEQPACKET	5		/* sequenced packet stream */
-	SOCK_SEQPACKET	5		/* sequenced packet stream */
+	SOCK_SEQPACKET	= 5		/* sequenced packet stream */
 //
 ///*
 // * Option flags per-socket.
