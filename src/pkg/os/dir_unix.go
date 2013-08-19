86dde8153632dad62ef4a825ca548ffded54dba9
Varialus <varialus@gmail.com>
2013-08-18 19:10:17 -0600

    Print readdirnames() d

--- src/pkg/os/dir_unix.go
+++ src/pkg/os/dir_unix.go
@@ -16,7 +16,7 @@ const (
 )
 
 func (f *File) readdirnames(n int) (names []string, err error) {
-	println("readdirnames() f.dirinfo ==", f.dirinfo)
+	//println("readdirnames() f.dirinfo ==", f.dirinfo)
 	// If this file has no dirinfo, create one.
 	if f.dirinfo == nil {
 		f.dirinfo = new(dirInfo)
@@ -24,6 +24,7 @@ func (f *File) readdirnames(n int) (names []string, err error) {
 		f.dirinfo.buf = make([]byte, blockSize)
 	}
 	d := f.dirinfo
+	println("readdirnames() d ==", d)
 
 	size := n
 	if size <= 0 {
