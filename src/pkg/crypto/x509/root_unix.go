31b00a776c3bac122207c68e2bab42c9c7237801
Varialus <varialus@gmail.com>
2013-08-21 20:58:50 -0600

    Add dfly references

--- src/pkg/crypto/x509/root_unix.go
+++ src/pkg/crypto/x509/root_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build freebsd dragonfly linux openbsd netbsd
+// +build dragonfly freebsd linux openbsd netbsd
 
 package x509
 
@@ -10,11 +10,13 @@ import "io/ioutil"
 
 // Possible certificate files; stop after finding one.
 var certFiles = []string{
-	"/etc/ssl/certs/ca-certificates.crt",     // Linux etc
-	"/etc/pki/tls/certs/ca-bundle.crt",       // Fedora/RHEL
-	"/etc/ssl/ca-bundle.pem",                 // OpenSUSE
-	"/etc/ssl/cert.pem",                      // OpenBSD
-	"/usr/local/share/certs/ca-root-nss.crt", // FreeBSD
+	"/etc/ssl/certs/ca-certificates.crt",       // Linux etc
+	"/etc/pki/tls/certs/ca-bundle.crt",         // Fedora/RHEL
+	"/etc/ssl/ca-bundle.pem",                   // OpenSUSE
+	"/etc/ssl/cert.pem",                        // OpenBSD
+	"/usr/local/share/certs/ca-root-nss.crt",   // FreeBSD
+	"/etc/cert/ca.pem",                         // DragonFly, Not in Base
+	"/usr/pkg/lib/python2.7/test/ssl_cert.pem", // DragonFly, Not in Base
 }
 
 func (c *Certificate) systemVerify(opts *VerifyOptions) (chains [][]*Certificate, err error) {
