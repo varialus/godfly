80729fbcb09af7b353c30493b8ac2585d99fdd62
Varialus <varialus@gmail.com>
2013-08-24 15:21:15 -0600

    Change error formatting

--- src/cmd/ld/ldelf.c
+++ src/cmd/ld/ldelf.c
@@ -695,7 +695,7 @@ ldelf(Biobuf *f, char *pkg, int64 len, char *pn)
 					goto bad;
 				sym.sym = symbols[info>>32];
 				if(sym.sym == nil) {
-					werrstr("error in src/cmd/ld/ldelf.c ldelf()\nsect->sym->name == %s\nj == %d\nreloc of invalid sym\n(int)(info>>32) == %d\nsym.name == '%s'\nsym.shndx == %d\nsym.type == %d\n\n",
+					werrstr("error in src/cmd/ld/ldelf.c ldelf()\nsect->sym->name is %s\nj is %d\nreloc of invalid sym\n(int)(info>>32) is %d\nsym.name is '%s'\nsym.shndx is %d\nsym.type is %d\n\n",
 						sect->sym->name, j, (int)(info>>32), sym.name, sym.shndx, sym.type);
 					//werrstr("%s#%d: reloc of invalid sym #%d %s shndx=%d type=%d",
 					//	sect->sym->name, j, (int)(info>>32), sym.name, sym.shndx, sym.type);
