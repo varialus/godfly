d944014af3575d5ea726081e8d45b985e3b0a4c1
Varialus <varialus@gmail.com>
2013-08-24 22:50:38 -0600

    Fixed error

--- src/cmd/6l/obj.c
+++ src/cmd/6l/obj.c
@@ -74,7 +74,7 @@ Header headers[] = {
 void
 main(int argc, char *argv[])
 {
-	printf("argv[0] == %s", argv[0]);
+	print("argv[0] == " + argv[0]);
 	Binit(&bso, 1, OWRITE);
 	listinit();
 	memset(debug, 0, sizeof(debug));
