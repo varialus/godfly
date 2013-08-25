8effed88ea7fbc6fdf7024cf5f5db3b8356d6e0d
Varialus <varialus@gmail.com>
2013-08-25 03:09:29 -0600

    Try to resolve undefined

--- src/pkg/os/user/lookup_stubs.go
+++ src/pkg/os/user/lookup_stubs.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build dragonfly,cgo
-// +build !cgo,!windows
+// +build dragonfly
+// +build cgo
 
 package user
 
